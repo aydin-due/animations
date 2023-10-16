3559c44d846a1b12e09a527b481d622bf5f8bf27
aydin :^) <a219201085@unison.mx>
2023-10-16 12:17:25 -0700

    Initial Commit

---
+++ animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  animations
+//
+//  Created by aydin salman on 16/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by aydin salman on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
