62f7a4cbb295de4f121b6111e2656aaf5618858a
aydin :^) <a219201085@unison.mx>
2023-10-17 10:44:54 -0700

    32 - explicit animations

--- animations/ContentView.swift
+++ animations/ContentView.swift
@@ -10,41 +10,43 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        print(animationAmount)
-        return VStack {
-            Stepper("scale amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                        .repeatCount(3, autoreverses: true)), in: 1...10)
-            Spacer()
-            Button("Tap Me") {
-                animationAmount += 1
+        // attach animation modifier to a view
+        
+        /*
+         Circle()
+             .stroke(.red)
+             .scaleEffect(animationAmount)
+             .opacity(2 - animationAmount)
+             .animation(
+                 .easeInOut(duration: 1)
+                 //.delay(1),
+                 //.repeatCount(3, autoreverses: true),
+                 .repeatForever(autoreverses: false),
+                value: animationAmount)
+        */
+        
+        // add animation modifier to a binding
+        
+        /*
+         Stepper("scale amount", value: $animationAmount.animation(
+             .easeInOut(duration: 1)
+                     .repeatCount(3, autoreverses: true)), in: 1...10)
+         */
+        
+        // explicitly animating state changes
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
             }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
-            //.blur(radius: (animationAmount - 1) * 3)
-            //.animation(.easeOut, value: animationAmount)
-            //.animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-            /*
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeInOut(duration: 1)
-                        //.delay(1),
-                        //.repeatCount(3, autoreverses: true),
-                        .repeatForever(autoreverses: false),
-                       value: animationAmount)
-            )
-            .onAppear {
-                animationAmount = 2
-            }
-             */
-        }
+       }
+       .padding(50)
+       .background(.red)
+       .foregroundColor(.white)
+       .clipShape(Circle())
+       .rotation3DEffect(
+        .degrees(animationAmount),
+                              axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+       )
         
     }
 }
