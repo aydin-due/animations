a75f0a1dfc2d804fdfe30d9de7768756bdc663fb
aydin :^) <a219201085@unison.mx>
2023-10-17 10:33:25 -0700

    32 - animation bindings

--- animations/ContentView.swift
+++ animations/ContentView.swift
@@ -10,32 +10,42 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me") {
-            //animationAmount += 1
+        print(animationAmount)
+        return VStack {
+            Stepper("scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                        .repeatCount(3, autoreverses: true)), in: 1...10)
+            Spacer()
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            //.blur(radius: (animationAmount - 1) * 3)
+            //.animation(.easeOut, value: animationAmount)
+            //.animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+            /*
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                        //.delay(1),
+                        //.repeatCount(3, autoreverses: true),
+                        .repeatForever(autoreverses: false),
+                       value: animationAmount)
+            )
+            .onAppear {
+                animationAmount = 2
+            }
+             */
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        //.scaleEffect(animationAmount)
-        //.blur(radius: (animationAmount - 1) * 3)
-        //.animation(.easeOut, value: animationAmount)
-        //.animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                    //.delay(1),
-                    //.repeatCount(3, autoreverses: true),
-                    .repeatForever(autoreverses: false),
-                   value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        
     }
 }
 
