f76e8e8de7fe8c101696968b68f6b6f73e7855ec
aydin :^) <a219201085@unison.mx>
2023-10-23 10:35:36 -0700

    33 - letters dragging animation

--- animations/ContentView.swift
+++ animations/ContentView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("ama m mie")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 200, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                // explicit animation animates just on ended (starting point)
-                .onEnded { _ in withAnimation(.spring) { dragAmount = CGSize.zero }}
-            )
-            // implicit animation animates both onchanged (dragging) & onended (start)
-            // .animation(.spring, value: dragAmount)
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) {
+                num in Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num)/20), value: dragAmount)
+            }
+        }
+        .gesture(DragGesture()
+            .onChanged { dragAmount = $0.translation }
+            .onEnded { _ in
+                dragAmount = .zero
+                enabled.toggle()
+            }
+        )
     }
 }
 
